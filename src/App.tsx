231e096448f10c296dd4f6596303405eb5dac369
Rony Cesana <9611986+rony76@users.noreply.github.com>
2025-11-18 00:18:03 +0100

    Color title with the Spanish flag colors

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import { VerbListContextProvider } from './hooks/VerbListContext';
 function App() {
   return (
     <>
-      <h1>Los verbos de los Giacominos</h1>
+      <h1>
+        Los verbos de los{' '}
+        <span style={{ color: '#AA151B' }}>Gia</span>
+        <span style={{ color: '#F1BF00' }}>comi</span>
+        <span style={{ color: '#AA151B' }}>nos</span>
+      </h1>
       <div className="container mt-4">
         <VerbListContextProvider>
           <SearchBarWrapper />
